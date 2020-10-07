277283d45d58ac05754a31801137e3230f7814d9
ruixing <ruixing.duan@capitalonline.net>
2020-10-07 19:40:33 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,17 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+/* $.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+}) */
+// 开发环境服务器地址
+var baseURL = "http://ajax.frontend.itheima.net";
+// 测试环境服务器地址
+// var baseURL = "http://ajax.frontend.itheima.net";
+
+$.ajaxPrefilter(function (params) {
+
+  params.url = baseURL + params.url;
+
+});
--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,58 @@ $(function () {
     $(".reg-box").hide();
   })
 
+  // 自定义验证规则
+  var form = layui.form;
+  // console.log(form);
+  form.verify({
+    pwd: [
+      /^[\S]{6,10}$/,
+      "密码必须6-10位，且不能是空格"
+    ],
+    repwd: function (val) {
+      var pwd = $(".reg-box input[name=password]").val()
+      console.log(val);
+      console.log(pwd);
+      
+      if (val !== pwd) {
+        return "两次密码输入不一致";
+      }
+    }
+  });
+
+  // 注册功能
+  $("#form_reg").on("submit", function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: 'http://ajax.frontend.itheima.net/api/reguser',
+      data: {
+        username: $(".reg-box [name=username]").val(),
+        password: $(".reg-box [name=password]").val(),
+      },
+      success: function (res) {
+        if (res.status != 0) {
+          return alert(res.message);
+        }
+        alert(res.message);
+      }
+    });
+  })
+
+  $("#form_login").submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/api/login",
+      data: $(this).serialize(),
+      success: function(res){
+        if(res.status !== 0 ){
+          return layer.msg(res.message);
+        }
+        layer.msg("恭喜您，登录成功");
+        localStorage.setItem("token",res.token);
+        location.href = "/index.html";
+      }
+    })
+  })
 })
