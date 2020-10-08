276eea6b4df9dea55ddd4a53feaa9740f0019c47
ruixing <ruixing.duan@capitalonline.net>
2020-10-08 15:58:39 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,6 +12,20 @@ var baseURL = "http://ajax.frontend.itheima.net";
 
 $.ajaxPrefilter(function (params) {
 
+  if (params.url.indexOf("/my/") !== -1) {
+    params.headers = {
+      Authorization: localStorage.getItem("token") || ""
+    }
+  }
+    params.complete = function(res){
+      // console.log(res.responseJSON);
+      var obj = res.responseJSON;
+      if(obj.status == 1 &&obj.message == "身份认证失败！"){
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+      }
+    }
+      
   params.url = baseURL + params.url;
 
 });
--- assets/js/login.js
+++ assets/js/login.js
@@ -28,7 +28,26 @@ $(function () {
   });
 
   // 注册功能
-  $("#form_reg").on("submit", function (e) {
+
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#link-login').click()
+    })
+  })
+  /* $("#form_reg").on("submit", function (e) {
     e.preventDefault();
     $.ajax({
       method: "POST",
@@ -44,19 +63,21 @@ $(function () {
         alert(res.message);
       }
     });
-  })
+  }) */
 
   $("#form_login").submit(function(e){
     e.preventDefault();
     $.ajax({
       method: "POST",
       url: "/api/login",
+      // 将一组表单元素编码为字符串以便提交，每个表单元素都必须有name属性
       data: $(this).serialize(),
       success: function(res){
         if(res.status !== 0 ){
           return layer.msg(res.message);
         }
         layer.msg("恭喜您，登录成功");
+        
         localStorage.setItem("token",res.token);
         location.href = "/index.html";
       }
